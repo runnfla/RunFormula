255244160bd6e7b27d67425b62256b32d251a796
runnfla <runfla@yandex.com>
2026-04-08 17:09:08 +0300

    New

---
+++ Demo/rfdemounit.pas
@@ -0,0 +1,271 @@
+//*****************************************************
+//  RunFormula Expression Parser and Evaluator
+//  RunFormula Demo and Test
+//  Rev. 8.04.2026
+
+//  Author: Alexander Torubarov
+//  Contact: runfla@yandex.com
+
+//  Filename: RFTestUnit.pas
+
+//  Copyright (C) 2026 Alexander Torubarov
+//  Licensed under the MIT License.
+//  See LICENSE file in the project root
+//  or copy at https://opensource.org for full
+//  license information.
+//*****************************************************
+
+// TODO -
+
+unit RFDemoUnit;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  SynEdit;
+
+type
+
+  { TDemoForm }
+
+  TDemoForm = class(TForm)
+    SaveCheckBox: TCheckBox;
+    VrtCheckBox: TCheckBox;
+    TestsButton: TButton;
+    CloseButton: TButton;
+    ExecButton: TButton;
+    Label2: TLabel;
+    ResultMemo: TMemo;
+    ParseButton: TButton;
+    Label1: TLabel;
+    FlaSynEdit: TSynEdit;
+    procedure ParseButtonClick(Sender: TObject);
+    procedure ExecButtonClick(Sender: TObject);
+    procedure TestsButtonClick(Sender: TObject);
+    procedure CloseButtonClick(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  DemoForm: TDemoForm;
+
+implementation
+
+uses
+  RunFormula, Variants;
+
+{$R *.lfm}
+
+{$include ../RunFormula/runflamsg.inc}
+{$include ../RunFormula/runfladef.inc}
+{$include ../RunFormula/runflalib.inc}
+
+{ TDemoForm }
+
+const TagName : array[TTag] of string = (
+  'Value',
+  'Var',
+  'Array',
+  'Bracket',
+  'Func',
+  'Call',
+  'Text',
+  'Local',
+  'Subr',
+  'None',
+  'Expr',
+  'Code',
+  'Assign',
+  'Equal',
+  'NotEqual',
+  'Greater',
+  'Less',
+  'NotLess',
+  'NotGreater',
+  'Concat',
+  'Plus',
+  'Minus',
+  'OR',
+  'XOR',
+  'Mult',
+  'Slash',
+  'AND',
+  'SHL',
+  'SHR',
+  'DIV',
+  'MOD',
+  'Scale',
+  'NOT',
+  'Negative' );
+
+function MyRunFlaVar(constref Name:string; out Save:boolean):Variant;
+begin
+  Result:=InputBox('', 'Get value for variable <'+Name+'>', '');
+  Save:=false;
+end;
+
+procedure TDemoForm.ParseButtonClick(Sender: TObject);
+const Digits: array[0..$F] of char = '0123456789ABCDEF';
+var Error : TRunFlaError;
+    cod, info, S : string;
+    sz, L : SizeInt;
+    p, bp, fin : PByte;
+    sp : PChar;
+begin
+  ResultMemo.Clear;
+  cod:=RunFlaParse(FlaSynEdit.Text, Error);
+  with Error do if Code<>OK then begin
+    ResultMemo.Text:='ERROR at Position '+IntToStr(Position)+': '+RunFlaErrorMsg[Code];
+    FlaSynEdit.SelStart:=Position+1;
+    FlaSynEdit.SelEnd:=Position+2;
+    exit;
+  end;
+  p:=PByte(cod);
+  fin:=p+PToken(p)^.Size;
+  while p<fin do with PToken(p)^ do begin
+    case Tag of
+      TagValue, TagText : sz:=Size;
+      TagVar, TagFunc, TagCall : sz:=VarTokenSize;
+      TagArray, TagBracket, TagLocal, TagSubr, TagNone, TagExpr, TagCode : sz:=ExprTokenSize;
+      else sz:=OpTokenSize
+    end;
+    SetLength(S, sz*3);
+    bp:=p;
+    sp:=pointer(S);
+    for L:=1 to sz do begin
+      sp^:=Digits[bp^ shr 4];
+      inc(sp);
+      sp^:=Digits[bp^ and $F];
+      inc(sp);
+      sp^:=#$20;
+      inc(sp);
+      inc(bp);
+    end;
+    ResultMemo.Append(S);
+    info:=IntToStr(p-PByte(cod))+': Tag '+TagName[Tag]+
+      ', Position '+IntToStr(Source)+', Token size '+IntToStr(sz);
+    if Tag<TagAssign then info:=info+', Next '+IntToStr(p-PByte(cod)+Size);
+    case Tag of
+      TagValue : with ValRec do case VType of
+                   VInt   : S:=': Int = '+IntToStr(Int);
+                   VFloat : S:=': Float = '+FloatToStr(Flo);
+                   VCplex : S:=': Complex = ('+FloatToStr(Flo)+','+FloatToStr(Img)+')';
+                   VChar  : begin
+                              S:=': Char = ';
+                              if (Chr>$20) and (Chr<$7F) then S:=S+'"'+char(Chr)+'" ';
+                              S:=S+'['+IntToStr(Chr)+']';
+                            end;
+                 end;
+      TagVar : S:=': ID '+IntToStr(Index)+' <'+
+                 string(fin+PSizeInt(fin+Index*SI+OpTokenSize)^+OpTokenSize)+'>';
+      TagFunc : S:=': Index '+IntToStr(Index);
+      TagCall : S:=': Goto '+IntToStr(p-PByte(cod)-Index);
+      TagText : S:=': Text "'+string(@Text)+'" (length '+IntToStr(length(string(@Text)))+')';
+      else SetLength(S, 0);
+    end;
+    ResultMemo.Append(info+S);
+    ResultMemo.Append('');
+    inc(p, sz);
+  end;
+end;
+
+function AppendResult(const ParamCount:SizeInt; Context:pointer):Variant;
+begin
+  DemoForm.ResultMemo.Text:=DemoForm.ResultMemo.Text+RunFlaParam(0-ParamCount, Context);
+end;
+
+procedure TDemoForm.ExecButtonClick(Sender: TObject);
+var FlaError : TRunFlaError;
+    Exec : string;
+begin
+  ResultMemo.Clear;
+  RunFlaFuncReg('appendresult', @AppendResult);
+  if VrtCheckBox.Checked
+  then Exec:=RunFlaExecVrt(RunFlaParse(FlaSynEdit.Text, FlaError), FlaError, @MyRunFlaVar)
+  else Exec:=RunFlaExecStr(RunFlaParse(FlaSynEdit.Text, FlaError), FlaError, @MyRunFlaVar);
+  with FlaError do if Code<>OK then begin
+    ResultMemo.Text:='ERROR at Position '+IntToStr(Position)+': '+RunFlaErrorMsg[Code];
+    FlaSynEdit.SelStart:=Position+1;
+    FlaSynEdit.SelEnd:=Position+2;
+    exit;
+  end;
+  ResultMemo.Append(Exec);
+end;
+
+procedure TDemoForm.TestsButtonClick(Sender: TObject);
+type
+    TFuncRec = record
+    ID : PByte;
+    IsUser : boolean;
+    case byte of
+      0 : (Func : TFlaFunc);
+      1 : (UserFunc : TRunFlaFunc);
+  end;
+  PFuncRec = ^TFuncRec;       bbb = type string;
+
+
+var T : TTag;
+    Token : TToken;
+    Info, S : string;
+    flg : boolean;
+    I : SizeInt;                P : pointer; C : char;
+    VT : TVType;                VVV : Variant;
+
+
+begin                                 // show all struc size
+  ResultMemo.Clear;
+  Info:='Tag Name Check: ';
+  flg:=true;
+  for T in TTag do begin
+    str(T, S);
+    if 'Tag'+TagName[T]<>S then begin
+      ResultMemo.Append(Info+'ERROR (Tag'+TagName[T]+' <=> '+S+')');
+      flg:=false;
+    end;
+  end;
+  if flg then ResultMemo.Append(Info+'OK');
+
+
+                      // !!! TEST Str2Ptr, DecStrRef
+
+  ResultMemo.Clear;
+  S:='Size of Char='+IntToStr(SizeOf(char))+', Integer='+IntToStr(SizeOf(integer))
+    +', SizeInt='+IntToStr(SizeOf(SizeInt));
+  ResultMemo.Append(S);
+  ResultMemo.Append('Size of TFloat: '+IntToStr(SizeOf(TRFloat)));
+  ResultMemo.Append('Size of AnsiRec (should be SizeInt*3): '+IntToStr(SizeOf(TAnsiRec)));
+  ResultMemo.Append('Size of TTag: '+IntToStr(SizeOf(TTag)));
+
+  SetLength(S, 1);                                       // test SetLength(S, 0)=nil
+  if Pointer(S)<>nil then begin
+    SetLength(S, 0);
+    flg:=Pointer(S)=nil;
+  end;
+  if flg then S:='OK' else S:='ERROR';
+  ResultMemo.Append('Test SetLength(S, 0) = nil: '+S);
+
+  SetLength(S, 2);                                       // test Pointer(S)=@S[1]
+  if @S[2]=(Pointer(S)+1) then S:='OK' else S:='ERROR';
+  ResultMemo.Append('Test Pointer(S) = @S[1]: '+S);
+
+  I:=PByte(@Token.Size)-PByte(@Token);                     // test OpToken Size
+  if I=OpTokenSize then S:='OK' else S:='ERROR';
+  ResultMemo.Append('Size of OpToken: '+IntToStr(I)+'; OpTokenSize: '+IntToStr(OpTokenSize));
+  ResultMemo.Append('Real OpToken size = OpTokenSize: '+S);
+
+  ResultMemo.Append('Done.');
+end;
+
+procedure TDemoForm.CloseButtonClick(Sender: TObject);
+begin
+  Close;
+end;
+
+end.
+
---
+++ RunFormula/runformula.pas
@@ -0,0 +1,427 @@
+//*****************************************************
+//  RunFormula Expression Parser and Evaluator
+//  Version 0.0.a
+//  Released at 8.04.2026
+
+//  Author: Alexander Torubarov
+//  Contact: runfla@yandex.com
+
+//  Filename: runformula.pas
+//  Source Code: Object Pascal / FreePascal
+//  Compatible: Lazarus x64
+
+//  Copyright (C) 2026 Alexander Torubarov
+//  See LICENSE file in the project root
+//  or copy at https://opensource.org for full
+//  license information.
+//*****************************************************
+
+// TODO -
+
+unit RunFormula;
+
+{$mode ObjFPC}{$H+}
+{$B-}                           // do not complete boolean evaluation
+{$inline on}
+
+interface
+
+type
+  TRFloat = Double;             // or Extended... or Single...
+
+  TRunFlaVar = function(constref Name:string; out Save:boolean):Variant;
+  TRunFlaFunc = function(const ParamCount:SizeInt; Context:pointer):Variant;
+
+{$include runflaerr.inc}
+{$include runflamsg.inc}
+
+function RunFlaParse(constref Fla:string; var Error:TRunFlaError):string;
+function RunFlaParse(constref Fla:string):string;
+function RunFlaExecStr(constref Fla:string; var Error:TRunFlaError; FlaVar:TRunFlaVar=nil):string;
+function RunFlaExecStr(constref Fla:string; FlaVar:TRunFlaVar=nil):string;
+function RunFlaExecVrt(constref Fla:string; var Error:TRunFlaError; FlaVar:TRunFlaVar=nil):Variant;
+function RunFlaExecVrt(constref Fla:string; FlaVar:TRunFlaVar=nil):Variant;
+function RunFlaParam(Offset:SizeInt; Context:pointer):Variant;
+procedure RunFlaRaise(ErrCode:TRunFlaErrCode);
+function RunFlaFuncReg(constref Name:string; Func:TRunFlaFunc):TRunFlaErrCode;
+
+var RunFlaErrCode : TRunFlaErrCode = OK;
+
+implementation
+
+{$define runfla_optuses}
+
+uses SysUtils
+
+{$include runflaopt.inc}
+
+{$undef runfla_optuses} ;
+
+{$include runfladef.inc}
+{$include runflalib.inc}
+
+function RunFlaParse(constref Fla:string; var Error:TRunFlaError):string;
+const TagOp = TagExpr;                     //DONE -oMain -cRev.2026.04.08: Func RunFlaParse
+type TCls = record
+       ClsTag : TTag;
+       SizeP : PSizeInt;                      // to Token.Size
+       Offs : SizeInt;
+     end;
+     PCls = ^TCls;
+var Buf : array of string;
+    SBuf : PByte = nil;
+    BufP : PByte = PByte(BufSize);
+    BufSum : SizeInt = 0;                          // sum of closed Bufs
+    ClsList, SubrList, DefList, VarList : TMemList;
+    PreTag : TTag = TagNone;
+    TextCodePage : TSystemCodePage;
+    InDef : integer = 0;
+    Comment : char = #0;
+    FlaPos : integer = 0;                    // position in Fla
+    Pnt : PChar;
+    ofs, i : SizeInt;
+
+{$include runflaparse.inc}
+
+begin
+  try
+    PByte(Result):=nil;
+    MemListInit(ClsList, SizeOf(TCls), ClsGrow);
+    MemListInit(SubrList, SFlaRec, SubrGrow, true);
+    MemListInit(DefList, SFlaRec, DefGrow, true);
+    MemListInit(VarList, PtrUInt(@TFlaRec(nil^).IsUser), VarIDGrow, true);
+    Pnt:=pointer(Fla);
+    if Pnt=nil then raise EError.Create(OK);
+    TextCodePage:=PAnsiRec(Pnt-SAnsiRec)^.CodePage;
+    ExprToken(TagCode, TagCode);
+    DoParse(Pnt);
+    if ClsList.Count>1 then raise EError.Create(MissingBracket);
+    if PreTag=TagCode then NoneToken;              // insert none if empty fla
+    CloseToken(TagCode);
+    OpToken(TagNone);                              // insert stub
+    RequestBuf(VarList.Count*SI);                  // writing Var Name Table
+    ofs:=VarList.Count*SI+SAnsiRec;
+    for i:=0 to VarList.Count-1 do begin
+      PSizeInt(BufP)^:=ofs;
+      inc(BufP, SI);
+      inc(ofs, AlignStringLng(PFlaRec(MemListGet(VarList, i))^.Lng));
+    end;
+    for i:=0 to VarList.Count-1 do with PFlaRec(MemListGet(VarList, i))^ do
+      WriteStringRec(ID, Lng, AlignStringLng(Lng));
+    i:=length(Buf)-1;
+    SetLength(Buf[i], BufP-SBuf);
+    if i>0 then begin
+      inc(BufSum, BufP-SBuf);
+      SetLength(Result, BufSum);
+      BufP:=PByte(Result);
+      for i:=0 to i do begin
+        ofs:=length(Buf[i]);
+        DataCopy(PByte(Buf[i]), BufP, ofs);
+        inc(BufP, ofs);
+      end;
+    end else Result:=Buf[0];
+    FillError(OK);
+  except
+    on E:EError do FillError(E.FCode, E.FPnt);
+    on E:EOverflow do FillError(Overflow);
+    on E:EHeapException do FillError(Malloc);
+    else FillError(Unknown);
+  end;
+  for i:=0 to DefList.Count-1 do DecStrRef(PFlaRec(MemListGet(DefList, i))^.PText);
+  MemListFree(VarList);
+  MemListFree(DefList);
+  MemListFree(SubrList);
+  MemListFree(ClsList);
+  for i:=0 to length(Buf)-1 do SetLength(Buf[i], 0);
+  SetLength(Buf, 0);
+end;
+
+function RunFlaParse(constref Fla:string):string;               //DONE -oMain -cRev.2026.03.31: Func RunFlaParse
+var Error : TRunFlaError;
+begin
+  Result:=RunFlaParse(Fla, Error);
+end;
+
+function Term(Pnt:PByte; var Context:TContext):PValRec;         //DONE -oMain -cRev.2026.04.08: Func Term
+var Status : TStatus;
+    lst : PMemList;
+    pv : PValRec;
+    p, fin, fn : PByte;
+    idx, i : SizeInt;
+    flg : boolean;
+
+{$include runflaterm.inc}
+
+begin
+  Context.ProcToken:=Pnt;;
+  with PToken(Pnt)^ do begin
+    case Tag of
+      TagValue : Result:=@ValRec;
+      TagVar   : begin
+                   for i:=Context.VarPool.Count-1 downto 0 do begin
+                     lst:=MemListGet(Context.VarPool, i);
+                     idx:=VarListFind(lst, Index);
+                     if idx>=0 then break;
+                   end;
+                   if idx<0 then begin
+                     if PToken(Pnt+VarTokenSize)^.Tag<>TagAssign then with Context do begin
+                       if RunFlaVar=nil then raise EError.Create(UnknownVar);
+                       Result:=Vrt2Val(RunFlaVar(string(VarTable+PSizeInt(VarTable+Index*SI)^), flg));
+                       if flg then ValCopy(Result, CreateNewVar(Pnt, false));
+                     end else Result:=CreateNewVar(Pnt);
+                   end else Result:=@PVariable(MemListGet(lst^, idx))^.Value;
+                 end;
+      TagArray   : raise EError.Create(Unsupported);
+      TagBracket : Result:=DoBracket(Pnt+ExprTokenSize, Pnt+Size);
+      TagFunc : begin
+                  p:=Pnt+(ExprTokenSize+SI);
+                  fin:=Pnt+Size;
+                  with Context do begin
+                    idx:=FuncArg.Count;
+                    while p<fin do begin
+                      PPByte(MemListAdd(FuncArg))^:=p;
+                      inc(p, PToken(p)^.Size);
+                    end;
+                    with PFlaRec(MemListGet(FuncList, Index))^ do if IsUser
+                      then Result:=Vrt2Val(UserFunc(FuncArg.Count-idx, @Context))
+                      else Result:=Func(FuncArg.Count-idx, Context);
+                    FuncArg.Count:=idx;
+                  end;
+                end;
+      TagCall : begin
+                  p:=Pnt+(ExprTokenSize+SI);
+                  fin:=Pnt+Size;
+                  SaveStatus(Status, Context);
+                  while p<fin do begin
+                    PPValRec(MemListAdd(Context.SubrParam))^:=Term(p, Context);
+                    inc(p, PToken(p)^.Size);
+                  end;
+                  p:=Pnt-Index;
+                  fin:=p+PToken(p)^.Size;
+                  inc(p, ExprTokenSize*2);
+                  fn:=p+PToken(p)^.Size;
+                  inc(p, ExprTokenSize);
+                  NewVarList(Context);
+                  i:=Status.CntSubrParam;
+                  while p<fn do begin
+                    pv:=nil;
+                    if i<Context.SubrParam.Count then pv:=PPValRec(MemListGet(Context.SubrParam, i))^;
+                    InitVar(p, pv, true);
+                    inc(p, PToken(p)^.Size);
+                    inc(i);
+                  end;
+                  if i<Context.SubrParam.Count then raise EError.Create(ParamNumber, Pnt);
+                  inc(Status.CntVarPool);
+                  RestoreStatus(Status, Context);      // restoring w/o new varlist
+                  dec(Status.CntVarPool);
+                  try
+                    Result:=DoBracket(p, fin);
+                  except
+                    on E:EFlaExit do Result:=Context.TermExit;
+                  end;
+                  if Result^.VAlloc<>CD
+                    then RestoreStatusKeep(Status, Context, Result)
+                    else RestoreStatus(Status, Context);
+                end;
+      TagText : begin
+                  Result:=NewLV(Context);
+                  Result^.VType:=Vstr;
+                  Result^.Str:=@Text;
+                end;
+      TagLocal : begin
+                   p:=Pnt+ExprTokenSize;
+                   fin:=Pnt+Size;
+                   while p<fin do begin
+                     InitVar(p, @CVNone, false);
+                     inc(p, PToken(p)^.Size);
+                   end;
+                   Result:=@CVNone;
+                 end;
+      TagSubr, TagNone : Result:=@CVNone;
+      TagExpr, TagCode : begin
+                           p:=Pnt+ExprTokenSize;
+                           Result:=Expr(p, Pnt+Size, 0);
+                         end;
+    end;
+  end;
+  Context.TermExit:=Result;
+end;
+
+function Param(Offset:SizeInt; var Context:TContext):PValRec;   //DONE -oMain -cRev.2026.03.28: Func Param
+var tkn : PByte;
+begin
+  with Context do begin
+    tkn:=ProcToken;
+    Result:=Term(PPByte(MemListGet(FuncArg, FuncArg.Count+Offset))^, Context);
+    ProcToken:=tkn;
+  end;
+end;
+
+function RunFlaParam(Offset:SizeInt; Context:pointer):Variant;  //DONE -oMain -cRev.2026.03.28: Func RunFlaParam
+type PContext = ^TContext;
+var tkn : PByte;
+begin
+  if Offset>=0 then raise EError.Create(ParamNumber);
+  with PContext(Context)^ do begin
+    tkn:=ProcToken;
+    Result:=AsVrt(Term(PPByte(MemListGet(FuncArg, FuncArg.Count+Offset))^, PContext(Context)^));
+    PostParam(PContext(Context)^);
+    ProcToken:=tkn;
+  end;
+end;
+
+function Exec(constref Fla:string; var Error:TRunFlaError; FlaVar:TRunFlaVar; Buf:PValRec):PValRec;
+const InitProc : TToken = (Tag : TagCode; Source : 0);     //DONE -oMain -cRev.2026.04.08: Func Exec
+var Context : TContext;
+    Status : TStatus;
+    p : PByte;
+    i : SizeInt;
+
+  procedure FillError(Err:TRunFlaErrCode; P:pointer=nil);     //DONE -oMain -cRev.2026.04.08: Proc FillError
+  begin
+    if Err<>OK then begin
+      if RunFlaErrCode=OK then RunFlaErrCode:=Err;
+      if Error.Code=OK then begin
+        Error.Code:=Err;
+        if P=nil then P:=Context.ProcToken;
+        Error.Position:=PToken(P)^.Source;
+      end;
+    end;
+  end;
+
+begin
+  Result:=@CVNone;
+  with Context do begin
+    MemListInit(VarPool, SizeOf(TMemList), VarPoolGrow);
+    MemListInit(LVStack, SValRec, LVStackGrow);
+    MemListInit(SubrParam, SPtr, ParamGrow);
+    MemListInit(FuncArg, SPtr, ParamGrow);
+    RunFlaVar:=FlaVar;
+    MaxVarPool:=0;
+    ProcToken:=@InitProc;
+  end;
+  SaveStatus(Status, Context);
+  try
+    p:=PByte(Fla);
+    if p=nil then raise EError.Create(OK);
+    Context.VarTable:=p+PToken(p)^.Size+OpTokenSize;
+    NewVarList(Context);
+    Result:=Term(p, Context);
+    FillError(OK);
+  except
+    on E:EError do FillError(E.FCode, E.FPnt);
+    on E:EFlaResult do begin
+      Result:=Context.TermExit;
+      FillError(OK);
+    end;
+    on E:EFlaExit do FillError(IllegalOp);
+    on E:EFlaBreak do FillError(IllegalOp);
+    on E:EFlaContinue do FillError(IllegalOp);
+    on E:EOverflow do FillError(Overflow);
+    on E:EDivByZero do FillError(DivZero);
+    on E:EZeroDivide do FillError(DivZero);
+    on E:EInvalidOp do FillError(InvalidValue);
+    on E:EHeapException do FillError(Malloc);
+    else FillError(Unknown);
+  end;
+  if Result^.VAlloc<>CD then begin
+    ValCopy(Result, Buf);
+    Result:=Buf;
+  end;
+  RestoreStatus(Status, Context);
+  with Context do begin
+    for i:=MaxVarPool-1 downto 0 do with PMemList(MemListGet(VarPool, i))^ do MemListFree(PMemList(@List)^);
+    MemListFree(FuncArg);
+    MemListFree(SubrParam);
+    MemListFree(LVStack);
+    MemListFree(VarPool);
+  end;
+end;
+
+function RunFlaExecStr(constref Fla:string; var Error:TRunFlaError; FlaVar:TRunFlaVar=nil):string;
+var R : TValRec;                                          //DONE -oMain -cRev.2026.03.28: Func RunFlaExecStr
+begin
+  Result:=Str2Str(AsStr(Exec(Fla, Error, FlaVar, @R)));
+  FreeValue(@R);
+end;
+
+function RunFlaExecStr(constref Fla:string; FlaVar:TRunFlaVar=nil):string;
+var Error : TRunFlaError = (Code : Unknown);              //DONE -oMain -cRev.2026.03.28: Func RunFlaExecStr
+    R : TValRec;             // prevent filling
+begin
+  Result:=Str2Str(AsStr(Exec(Fla, Error, FlaVar, @R)));
+  FreeValue(@R);
+end;
+
+function RunFlaExecVrt(constref Fla:string; var Error:TRunFlaError; FlaVar:TRunFlaVar=nil):Variant;
+var R : TValRec;                                          //DONE -oMain -cRev.2026.03.28: Func RunFlaExecVrt
+begin
+  Result:=AsVrt(Exec(Fla, Error, FlaVar, @R));
+  FreeValue(@R);
+end;
+
+function RunFlaExecVrt(constref Fla:string; FlaVar:TRunFlaVar=nil):Variant;
+var Error : TRunFlaError = (Code : Unknown);              //DONE -oMain -cRev.2026.03.28: Func RunFlaExecVrt
+    R : TValRec;             // prevent filling
+begin
+  Result:=AsVrt(Exec(Fla, Error, FlaVar, @R));
+  FreeValue(@R);
+end;
+
+procedure RunFlaRaise(ErrCode:TRunFlaErrCode);            //DONE -oMain -cRev.2026.03.28: Proc RunFlaRaise
+begin
+  raise EError.Create(ErrCode);
+end;
+
+function FuncReg(constref Name:string; FlaFunc:TFlaFunc; User:boolean=false):TRunFlaErrCode;
+var fr : PFlaRec;                                         //DONE -oMain -cRev.2026.04.08: Func FuncReg
+    p : PByte;
+    L, i : SizeInt;
+begin
+  try
+    p:=Str2Ptr(Name);
+    L:=PSizeInt(p-SI)^;
+    i:=MemListFind(FuncList, p, p+L);
+    if i<0 then begin
+      fr:=MemListInsert(FuncList, not i);
+      Result:=OK;
+    end else begin
+      fr:=MemListGet(FuncList, i);
+      DecStrRef(fr^.ID);
+      Result:=FuncExists;
+    end;
+    with fr^ do begin
+      ID:=p;
+      Lng:=L;
+      IsUser:=User;
+      Func:=FlaFunc;
+    end;
+  except
+    on E:EHeapException do Result:=Malloc;
+    else Result:=Unknown;
+  end;
+  if (Result<>OK) and (RunFlaErrCode=OK) then RunFlaErrCode:=Result;
+end;
+
+function RunFlaFuncReg(constref Name:string; Func:TRunFlaFunc):TRunFlaErrCode;
+begin                                                     //DONE -oMain -cRev.2026.03.28: Func RunFlaFuncReg
+  Result:=FuncReg(Name, TFlaFunc(Func), true);
+end;
+
+{$include runflafunc.inc}
+
+initialization
+
+  MemListInit(FuncList, SFlaRec, FuncGrow, true);
+  FuncRegister;
+
+finalization
+
+  while FuncList.Count>0 do begin
+    DecStrRef(PFlaRec(MemListGetLast(FuncList))^.ID);
+    dec(FuncList.Count);
+  end;
+  MemListFree(FuncList);
+
+end.
+
